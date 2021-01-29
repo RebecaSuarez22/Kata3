d5314a9efe29549ffdb07f02b7b27ad129be6f60
Rebeca <>
2021-01-29 13:47:56 +0000

    Kata3

---
+++ src/HistogramDisplay.java
@@ -0,0 +1,47 @@
+package Kata3;
+
+/**
+ *
+ * @author Rebeca
+ */
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame {
+    private final Histogram<String> histogram;
+    public HistogramDisplay(String title, Histogram<String> histogram) {
+        super(title);
+        this.histogram = histogram;
+        this.setContentPane(createPanel());
+        this.pack();
+    }
+
+    private JPanel createPanel() {
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataSet()));
+        return chartPanel;
+    }
+
+    private JFreeChart createChart(DefaultCategoryDataset dataSet) {
+        JFreeChart chart = ChartFactory.createBarChart("JFreeChart Histogram", "email domains", "nº emails", 
+                dataSet, PlotOrientation.VERTICAL, 
+                false, false, rootPaneCheckingEnabled);
+        return chart;
+    }
+    
+    private DefaultCategoryDataset createDataSet() {
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }
+        return dataSet;
+    }
+    
+    public void execute() {
+        this.setVisible(true);
+    }
+}
---
+++ src/Kata3.java
@@ -0,0 +1,14 @@
+package kata3;
+
+/**
+ *
+ * @author Rebeca
+ */
+public class Kata3 {
+    
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay("HISTOGRAMA");
+        histo.execute();
+    }
+    
+}
